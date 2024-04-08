3cd0b9edddf92cde4553c5c7095ceaf3e80bb9ac
frederico-klein <frekle@kth.se>
2024-04-08 12:23:50 +0200

    seems to work

--- src/simple_clock.cpp
+++ src/simple_clock.cpp
@@ -17,9 +17,6 @@ custom_clock::custom_clock()
 {
 	ros::NodeHandle n("~");
 	clock_pub = n.advertise<rosgraph_msgs::Clock>("/clock", 10);
-	start_clock_srv = n.advertiseService("start", &custom_clock::start, this);
-	stop_clock_srv = n.advertiseService("stop", &custom_clock::stop, this);
-	n.param<bool>("start_playing", playing, true);
 	n.param<int>("clock_step_microsseconds", clock_step_microsseconds, 1000);
 	int slowdown_rate;
 	//DEFAULT: run 100 times slower than normal clock
@@ -41,35 +38,7 @@ custom_clock::custom_clock()
 	current_faketime = start_time;
 }
 
-bool custom_clock::start(std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &res )
-{
-	playing = true;
-	return true;	
-}
-bool custom_clock::stop(std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &res )
-{
-	playing = false;
-	return true;	
-}
-bool custom_clock::step(std_srvs::EmptyRequest &req, std_srvs::EmptyResponse &res )
-{
-	pub_once_different_time();
-	return true;	
-}
 
-
-void custom_clock::pub_same_time_at_different_rate()
-{
-	/*
-	 *	Published the same time, but at any given rate
-	 * */
-	rosgraph_msgs::Clock clock_msg;
-	//gets wall time
-	boost::chrono::system_clock::time_point t = boost::chrono::system_clock::now();
-	clock_msg.clock.fromNSec(t.time_since_epoch().count());
-	clock_pub.publish(clock_msg);
-
-}
 void custom_clock::pub_once_different_time()
 {
 	rosgraph_msgs::Clock clock_msg;
@@ -81,15 +50,8 @@ void custom_clock::pub_once_different_time()
 }
 void custom_clock::sleep()
 {
-	//lets try to make this more accurate. 
-	boost::chrono::system_clock::rep before_spin = boost::chrono::system_clock::now().time_since_epoch().count();
-	ros::spinOnce(); //If I want to be able to stop and start I need to spin it. 
-	boost::chrono::system_clock::rep after_spin = boost::chrono::system_clock::now().time_since_epoch().count();
-	uint64_t time_difference = after_spin-before_spin;
-	//std::cout << time_difference << std::endl;
-	boost::chrono::nanoseconds sleep_amount_ns(real_sim_clock_between_msgs_duration-time_difference);
+	boost::chrono::nanoseconds sleep_amount_ns(real_sim_clock_between_msgs_duration);
 	boost::this_thread::sleep_for( sleep_amount_ns );
-
 }
 
 custom_clock::~custom_clock()
@@ -98,14 +60,12 @@ custom_clock::~custom_clock()
 
 int main(int argc, char **argv)
 {
-	ros::init(argc, argv, "custom_clock");
+	ros::init(argc, argv, "simpler_clock");
 	custom_clock c;
 	//set fake rate:
 	while(ros::ok())
 	{
-		if (c.playing)
-			c.pub_once_different_time();
-		//c.pub_same_time_at_different_rate();
+		c.pub_once_different_time();
 		c.sleep();	
 	}
 	return 0;
